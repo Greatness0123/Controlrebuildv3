840bd6e8b56fa31b0a1023baf4d7343fb6062533
Greatness0123 <grucookorie@gmail.com>
2026-03-27 13:26:34 +0100

    commit

--- control_web/schema_ai_status.sql
+++
@@ -1,3 +0,0 @@
--- Add ai_status to chat_sessions
-ALTER TABLE public.chat_sessions 
-    ADD COLUMN IF NOT EXISTS ai_status TEXT DEFAULT 'idle' CHECK (ai_status IN ('idle', 'running', 'paused', 'stopped'));
